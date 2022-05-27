bd379cdd402cbc982442e95eef1b979445aec0e8
Ben Lovy <ben@deciduously.com>
2022-05-27 10:20:18 -0400

    static-bash-arm64-macos.sh

---
+++ static-bash-arm64-macos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+
+nproc=$(sysctl -n hw.ncpu)
+mirror=http://ftpmirror.gnu.org
+
+# keyring
+keyring="gnu-keyring.gpg"
+keyring_url="$mirror"/gnu/"$keyring"
+if [ ! -f "$keyring" ]; then
+	curl -OL "$keyring_url"
+	gpg --import "$keyring"
+fi
+
+# Download if not exists
+version=5.1.16
+name=bash
+pkg="$name"-"$version"
+tarball="$pkg".tar.gz
+sig="$tarball".sig
+base_url="$mirror"/gnu/"$name"
+tarball_url="$base_url"/"$tarball"
+sig_url="$base_url"/"$sig"
+if [ ! -f "$tarball" ]; then
+	rm -f "$sig"
+	curl -OL "$tarball_url"
+	curl -OL "$sig_url"
+	gpg --verify "$sig" "$tarball"
+	rm "$sig"
+fi
+
+# Unpack
+rm -rf "$pkg"
+tar xf "$tarball"
+
+# Compile
+pushd "$pkg"
+./configure --enable-static   
+make -j"$nproc"
+popd
+
+# Package
+tar -C "$pkg" -cJf "$pkg"-arm64-macos.tar.xz "$name"
+
+# Cleanup
+rm -rf "$pkg"

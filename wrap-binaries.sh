8e26a5d8acdb1dab09766b3a510ae243c4e5abbf
Ben Lovy <ben@deciduously.com>
2022-06-29 13:18:22 -0400

    Fix executable detection logic

--- wrap-binaries.sh
+++ wrap-binaries.sh
@@ -7,19 +7,6 @@ export PREFIX="$PWD"/bootstrap_toolchain
 lib_dir=$PREFIX/usr/lib
 dynamic_linker=$PREFIX/usr/lib/ld-linux-aarch64.so.1
 triple=aarch64-unknown-linux-gnu
-elf_magic_bytes=$(echo '\0x7f\0x45\0x4c\0x46' | xxd -r)
-
-is_elf() {
-   [ ! -d "$1" ] && [ "$(head -c 4 "$1")" = "$elf_magic_bytes" ]
-}
-
-is_dynamic() {
-  ldd "$1" >/dev/null 2>&1
-}
-
-patch() {
-  "$PREFIX"/usr/bin/patchelf --set-interpreter "$dynamic_linker" --set-rpath "$lib_dir" "$1"
-}
 
 wrap() {
   # Rename file
@@ -38,14 +25,8 @@ EOF
 }
 
 wrap_all_elfs() {
-  # This function searches for all dyanmically-linked elf binaries in the provided dir
-  cd "$1"
-  for FILE in *; do
-    if is_elf "$FILE" && is_dynamic "$FILE" && [ ! "$FILE" = liblto_plugin.so ] && [ ! "$FILE" = ld.so ]; then
-      wrap "$FILE"
-    fi
-  done
-  cd -
+  # This function wraps all executable binaries in the provided dir, excluding shell scripts
+  find "$1" -type f -executable -exec file -i '{}' \; | grep 'x-executable; charset=binary' | while read line; do wrap $line; done
 }
 
 wrap_all_elfs "$PREFIX"/usr/bin
